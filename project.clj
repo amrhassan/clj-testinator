73fda7007929c7e0d7c58580fa217a6c1cec98ae
Amr Hassan <amr.hassan@gmail.com>
2018-08-05 20:51:46 +0200

    renamed to testinator

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-testinator "0.1.0-SNAPSHOT"
+(defproject testinator "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/amrhassan/clj-testinator"
   :license {:name "MIT"}
